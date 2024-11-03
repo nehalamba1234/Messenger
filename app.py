b31397ede85d3c1e21ab36f11f136b08964b26c2
Mohit <palmohit897@gmail.com>
2024-11-03 19:55:37 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template,request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room
 
 app = Flask(__name__)
